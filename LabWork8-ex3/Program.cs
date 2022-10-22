574b3e9dcbc76ba9d14c1f5b78ecbd03a65a7dc4
barinskaya <1388zxc@gmail.com>
2022-10-22 14:46:42 +0300

    Добавьте файлы проекта.

---
+++ LabWork8-ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LabWork8_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x="); //вывод на консоль
+            double x = double.Parse(Console.ReadLine()); //ввод и задание переменной вещественного типа
+
+            Console.WriteLine("Введите y="); //вывод на консоль
+            double y = double.Parse(Console.ReadLine());//ввод и задание переменной вещественного типа
+
+            double r = Math.Pow(Math.Pow(3, 2) + Math.Pow(3, 2), 0.5); //ввод и задание переменной вещественного типа
+
+            double a = Math.Pow(Math.Pow(x, 2) + Math.Pow(y, 2), 0.5); //задание переменной вещественного типа по формуле
+
+            if (a < r && x >= -3 && x <= 3 && y <= 3 && y >= 0) //проверка выполнения условия
+                Console.WriteLine("Точка попадает в заштрихованную область");//вывод на консоль
+            else //другой исход
+                Console.WriteLine("Точка не попадает в заштрихованную область");//вывод на консоль
+        }
+    }
+}
